0d2882426588142520ae2c49ac7379f9896ff030
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-11 19:59:01 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name="kishore";
+        byte age =19;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
