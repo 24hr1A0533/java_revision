c3fb004abf5c575af6c063d851efa4ed381eacaf
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-11 20:04:55 +0530

    Add files via upload

---
+++ PrimitiveDataType.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        byte a = 12;
+        short b = 133;
+        int c = 224;
+        long d = 143;
+        boolean flag1 = true;
+        boolean flag2 = false;
+        float ff = 12.9f;
+        double dd = 12.9d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(ff);
+        System.out.println(dd);
+        System.out.println(flag1);
+        System.out.println(flag2);
+    }
+}
