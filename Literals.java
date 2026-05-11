e47093bcd796eec09f9588507f7260586ce919f8
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-11 20:19:19 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+    static void main(String[] args) {
+        int num=999;
+        int num2=0b101;
+        int num3=0x5e;
+        int num4=10_00_00_00;
+        double value=2e3;
+        char c='a';
+        float f=0.1f;
+        double dd=0.111d;
+        boolean b=true;
+        boolean b1=false;
+        System.out.println(++c);
+        System.out.println(value);
+    }
+}
