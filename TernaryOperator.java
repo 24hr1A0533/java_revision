c710d3eb32cd52609a1c918d868f898543e2e20f
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-12 15:49:37 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int num = 5;
+        String result = num%2==0 ? "even" : "odd" ;
+        System.out.println(result);
+
+    }
+}
