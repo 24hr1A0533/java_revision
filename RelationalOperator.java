612955f3a9f61e8c78919bf8990511a9886130d4
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-12 14:41:29 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,12 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 18;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
