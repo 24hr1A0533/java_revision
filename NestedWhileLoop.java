d5f34107c12e424dddb68309a9a8854103fdce42
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-12 16:50:45 +0530

    Add files via upload

---
+++ NestedWhileLoop.java
@@ -0,0 +1,14 @@
+public class NestedWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<5) {
+            System.out.println("hello world i am AI");
+            int j = 1;
+            while (j <= 2) {
+                System.out.println("No need Bye AI");
+                j++;
+            }
+            i++;
+        }
+    }
+}
