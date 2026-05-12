42386bafe9427899b8d074e82752ef83b5c478a3
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-12 14:26:45 +0530

    Add files via upload

---
+++ ArthamaticOperators.java
@@ -0,0 +1,15 @@
+public class ArthamaticOperators {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println((float)a/b);
+        System.out.println(a%b);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+    }
+}
