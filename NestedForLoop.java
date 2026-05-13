24ea161a5ff43cd5db63e8cda9188e4144fa7ca9
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-05-13 17:59:37 +0530

    Add files via upload

---
+++ NestedForLoop.java
@@ -0,0 +1,11 @@
+public class NestedForLoop {
+    public static void main(String[] args) {
+        for (int i=1;i<=2;i++){
+            System.out.println("i love nature ");
+            int j=1;
+            for (;j<=2;j++){
+                System.out.println(" love my mom ");
+            } System.out.println();
+        }
+    }
+}
